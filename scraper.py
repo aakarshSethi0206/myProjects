9361065fb2fa307d2bac12121edd513f87ab5207
Aakarsh Sethi <aakarshsethi@Aakarshs-MacBook-Pro.local>
2025-12-29 23:27:26 +1100

    updated to scrape multiple pages

--- scraper.py
+++ scraper.py
@@ -2,17 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-def scrapeQuotes():
-    pageToScrape = requests.get("https://quotes.toscrape.com/")
-    soup = BeautifulSoup(pageToScrape.text, "html.parser")
-    authors = soup.findAll('small', attrs= {'class':'author'})
-    quotes = soup.findAll('span', attrs= {'class':'text'})
-    
-    with open("quotes.csv", mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Author", "Quote"])  
+url = 'https://quotes.toscrape.com/'
 
-        for author, quote in zip(authors, quotes):
-            writer.writerow([author.text, quote.text])
+def scrapeQuotes(url):
+    while url is not None:
+        pageToScrape = requests.get(url)
+        soup = BeautifulSoup(pageToScrape.text, "html.parser")
+        authors = soup.findAll('small', attrs= {'class':'author'})
+        quotes = soup.findAll('span', attrs= {'class':'text'})
+            
+            
 
-scrapeQuotes()
+        with open("quotes.csv", mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Author", "Quote"])  
+
+            for author, quote in zip(authors, quotes):
+                writer.writerow([author.text, quote.text])
+
+        nextButton = soup.find("li", attrs= {'class':'next'})
+        if nextButton:
+            nextPage = nextButton.find("a")["href"]
+            url = "https://quotes.toscrape.com" + nextPage
+        else:
+            url = None
+
+
+
+
+scrapeQuotes(url)
+
+
